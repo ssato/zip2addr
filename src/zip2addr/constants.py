227c8ce2bf1edda1e5beb15ddeaf5852c4093a8b
Satoru SATOH <satoru.satoh@gmail.com>
2023-01-22 01:26:17 +0900

    change: add ZIPCODE_{ZIP,CSV}_FILENAMES to reduce arguments given to zip2addr.datasrc.*

--- src/zip2addr/constants.py
+++ src/zip2addr/constants.py
@@ -8,9 +8,17 @@ VERSION: tuple[int, int, int] = (0, 1, 0)
 # .. seealso:: https://www.post.japanpost.jp/zipcode/download.html
 ROMAN_ZIPCODE_ZIP_FILENAME: typing.Final[str] = "ken_all_rome.zip"
 KANA_ZIPCODE_ZIP_FILENAME: typing.Final[str] = "ken_all.zip"
+ZIPCODE_ZIP_FILENAMES: tuple[str, ...] = (
+    ROMAN_ZIPCODE_ZIP_FILENAME,
+    KANA_ZIPCODE_ZIP_FILENAME,
+)
 
 ROMAN_ZIPCODE_FILENAME: typing.Final[str] = "KEN_ALL_ROME.csv"
 KANA_ZIPCODE_FILENAME: typing.Final[str] = "KEN_ALL.CSV"
+ZIPCODE_CSV_FILENAMES: tuple[str, ...] = (
+    ROMAN_ZIPCODE_FILENAME,
+    KANA_ZIPCODE_FILENAME,
+)
 
 JSON_FILENAME: typing.Final[str] = "zipcodes.json"
 
