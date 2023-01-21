07c266474127f8db8a66c60d77002323b01d0f3e
Satoru SATOH <satoru.satoh@gmail.com>
2023-01-21 23:15:51 +0900

    change: add some global variables because I prefer file name rather than path, etc.

--- src/zip2addr/constants.py
+++ src/zip2addr/constants.py
@@ -3,11 +3,16 @@
 import typing
 
 
-# .. seealso:: https://www.post.japanpost.jp/zipcode/download.html
-ROMAN_ZIPCODE_FILENAME: str = "KEN_ALL_ROME.csv"
-KANA_ZIPCODE_FILENAME: str = "KEN_ALL.CSV"
+VERSION: tuple[int, int, int] = (0, 1, 0)
 
-JSON_FILEPATH: typing.Final[str] = "zipcodes.json"
+# .. seealso:: https://www.post.japanpost.jp/zipcode/download.html
+ROMAN_ZIPCODE_ZIP_FILENAME: typing.Final[str] = "ken_all_rome.zip"
+KANA_ZIPCODE_ZIP_FILENAME: typing.Final[str] = "ken_all.zip"
+
+ROMAN_ZIPCODE_FILENAME: typing.Final[str] = "KEN_ALL_ROME.csv"
+KANA_ZIPCODE_FILENAME: typing.Final[str] = "KEN_ALL.CSV"
+
+JSON_FILENAME: typing.Final[str] = "zipcodes.json"
 
 # .. seealso:: https://peps.python.org/pep-0591/
 DATABASE_FILENAME: typing.Final[str] = "zipcodes.db"
