02e5385efca51a59ff9168fcde4c27087bf513cd
Satoru SATOH <satoru.satoh@gmail.com>
2023-01-22 16:46:47 +0900

    enhancement: add search sub command

--- src/zip2addr/cli.py
+++ src/zip2addr/cli.py
@@ -2,21 +2,25 @@
 # SPDX-License-Identifier: MIT
 """CLI frontend to manage database files.
 """
-import pathlib
+import pprint
 
 import click
 
-from . import constants, datagen
+from . import constants, iapi, utils
 
 
 @click.group()
-def main():
+@click.option("--verbose/--quiet", "-v/-q", default=False)
+def main(verbose: bool):
     """main entry point.
     """
+    if verbose:
+        iapi.set_verbose_mode()
 
 
 # ref.
-# - https://click.palletsprojects.com/en/8.1.x/options/#multi-value-options
+# https://click.palletsprojects.com/en/8.1.x/options/#multi-value-options
+# https://click.palletsprojects.com/en/8.1.x/utils/#pager-support
 @main.command()
 @click.option("--datadir", "-d", default='.')
 @click.option("--output", "-o", default=constants.DATABASE_FILEPATH)
@@ -33,12 +37,27 @@ def initdb(
     zip_filenames: tuple[str, str],
     csv_filenames: tuple[str, str],
 ):
-    """Prase csv files and dump its result as a database file.
     """
-    opath = pathlib.Path(output)
-    (outdir, outname) = (opath.parent, opath.name)
-
-    datagen.make_database_from_zip_files(
-        pathlib.Path(datadir), outdir, zip_filenames=zip_filenames,
-        csv_filenames=csv_filenames, outname=outname
+    Prase csv files extracted from zip files, save resutl data as a db file.
+    """
+    iapi.initdb(
+        datadir, output,
+        zip_filenames=zip_filenames, csv_filenames=csv_filenames
     )
+
+
+@main.command()
+@click.option("--db-path", "-d", default=constants.DATABASE_FILEPATH)
+@click.argument("zipcode")
+def search(db_path: str, zipcode: str):
+    """
+    Search address info from rhe database file by given zip code.
+    """
+    res = iapi.search_by_zipcode(zipcode, db_path, limit=0)
+    fmt = pprint.pformat
+
+    if len(res) > utils.get_term_lines():
+        click.echo_via_pager(fmt(r) for r in res)
+    else:
+        for zipd in res:
+            click.echo(fmt(zipd))
