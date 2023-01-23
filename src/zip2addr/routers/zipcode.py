1c7baee443c7345452872ac9bc60fca2a7efe10b
Satoru SATOH <satoru.satoh@gmail.com>
2023-01-23 19:54:22 +0900

    fix: make them works

--- src/zip2addr/routers/zipcode.py
+++ src/zip2addr/routers/zipcode.py
@@ -1,8 +1,8 @@
 """Routers.
 """
-import typing
-
 import fastapi
+import fastapi.encoders
+import fastapi.responses
 import sqlalchemy
 import sqlalchemy.orm
 
@@ -27,7 +27,8 @@ async def get_zipcodes(
     return crud.get_zipcodes(dbs, skip=skip, limit=limit)
 
 
-@ROUTER.get("/zipcodes/{zipcode}", response_model=schemas.Zipcode)
+# @ROUTER.get("/zipcodes/{zipcode}", response_model=schemas.Zipcode)
+@ROUTER.get("/zipcodes/{zipcode}")
 async def get_zipcode(
     zipcode: str,
     dbs: sqlalchemy.orm.Session = fastapi.Depends(db.get_default_session)
@@ -37,14 +38,15 @@ async def get_zipcode(
     res = crud.get_zipcode(dbs, zipcode)
     if res is None:
         raise fastapi.HTTPException(
-            status_code=404,
+            status_code=fastapi.status.HTTP_404_NOT_FOUND,
             detail=f"Zip code was not found: {zipcode}"
         )
-    return res
+    data = fastapi.encoders.jsonable_encoder(res.as_dict())
+    return fastapi.responses.JSONResponse(content=data)
 
 
 @ROUTER.get(
-    "/zipcodes/{partial_zipcode}",
+    "/zipcodes/partial/{partial_zipcode}",
     response_model=list[schemas.Zipcode]
 )
 async def get_zipcodes_by_partial_zipcode(
